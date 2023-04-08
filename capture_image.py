b91e1a45669e1fc58acb569d3d234421fb613218
ThakurSneha <thakursneha578@gmail.com>
2023-04-09 00:52:19 +0530

    Added Successfully

--- capture_image.py
+++ capture_image.py
@@ -5,16 +5,16 @@ import uuid
 
 IMAGE_PATH='CollectedImages'
 
-labels=['Blured','Blinding']
+labels=['Blinding', 'Blured', 'Fine']
 
-number_of_images=15
+number_of_images=20
 
 for label in labels:
     img_path = os.path.join(IMAGE_PATH, label)
     os.makedirs(img_path)
     cap=cv2.VideoCapture(0)
     print('Collecting images for {}'.format(label))
-    time.sleep(5)
+    time.sleep(10)
     for imgnum in range(number_of_images):
         ret,frame=cap.read()
         imagename=os.path.join(IMAGE_PATH,label,label+'.'+'{}.jpg'.format(str(uuid.uuid1())))
